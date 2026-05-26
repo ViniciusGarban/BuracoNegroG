2ce30fbaa4dd448444c6edca55d91b064e84e6c3
ViniciusGarban <vinicius.garban@sptech.school>
2026-05-25 22:35:58 -0300

    Aplicando Rota

--- Projeto Buraco Negro/web-data-viz-main/src/models/checkModel.js
+++ Projeto Buraco Negro/web-data-viz-main/src/models/checkModel.js
@@ -1,46 +1,20 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
 function buscarChecklist(fk_usuario, fk_capitulo) {
 
-    var instrucaoSql = `SELECT 
-        select fk_capitulo as capitulo, fk_usuario as usuario, lido FROM usuario_capitulo
-JOIN capitulo ON usuario_capitulo.fk_capitulo = ${fk_capitulo} 
-JOIN usuario ON usuario_capitulo.fk_usuario = ${fk_usuario};`
+    var instrucaoSql = `        
+        SELECT fk_capitulo, fk_usuario, lido
+        FROM usuario_capitulo
+        WHERE fk_usuario = ${fk_usuario}
+        AND fk_capitulo = ${fk_capitulo};
+`
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+   buscarChecklist
 }
--- Projeto Buraco Negro/web-data-viz-main/src/routes/check.js
+++ Projeto Buraco Negro/web-data-viz-main/src/routes/check.js
@@ -3,16 +3,8 @@ var router = express.Router();
 
 var checkController = require("../controllers/checkController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
-});
-
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
-
-router.get("/checklist/:fk_usuario", function (req, res) {
-    checkController.buscarCheck(req, res);
+router.get("/checklist/:fk_usuario/:fk_capitulo", function (req, res) {
+    checkController.buscarChecklist(req, res);
 })
 
 
